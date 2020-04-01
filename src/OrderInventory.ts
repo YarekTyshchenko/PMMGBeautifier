8b03b3c25c1e6f0ae93fcb723fc3de51ffb15bee
Trey <treycorple@gmail.com>
2020-04-01 08:41:03 -0500

    possibly fixed memory leak

--- src/OrderInventory.ts
+++ src/OrderInventory.ts
@@ -1,15 +1,28 @@
+import { genericCleanup } from "./util";
+
 /**
  * Sort Inventories by Code
  * TODO: only grab the inventory where the button was pressed
  */
 export class OrderInventory {
   private sortByCode: boolean = false;
-  cleanup() {}
+  private tag = "sort-by-code";
+  private cleanups: Array<() => void> = [];
+
+  cleanup() {
+    this.cleanups.forEach((f, i) => {
+      f();
+      delete this.cleanups[i];
+    });
+    genericCleanup(this.tag);
+  }
 
   run() {
     this.sortInventoriesByCode();
     this.addSortByCodeButton();
   }
+
+
 
   private sortInventoriesByCode() {
     if (this.sortByCode) {
@@ -42,12 +55,20 @@ export class OrderInventory {
     }
   }
 
+
+
   private addSortByCodeButton() {
     const buttonContainer = Array.from(
       document.querySelectorAll(
         "div[class^='InventorySortControls__controls___'"
       )
     );
+    const sortByCodeOff = () => {
+      this.sortByCode = false
+    }
+    const sortByCodeOn = () => {
+      this.sortByCode = true
+    }
     buttonContainer.forEach(e => {
       let buttons = Array.from(e.children);
       let numButtons = buttons.length;
@@ -55,25 +76,28 @@ export class OrderInventory {
         let sortingButtons = buttons.splice(1, 5);
         console.log(sortingButtons);
         sortingButtons.forEach(button => {
-          button.addEventListener("click", () => {
-            this.sortByCode = false;
-    
-          });
+          button.addEventListener("click", sortByCodeOff);
         });
         let codeButton = document.createElement("div");
         codeButton.classList.add("InventorySortControls__criteria___1UBEZGp");
+        codeButton.classList.add(this.tag);
         let title = document.createElement("div");
+        title.classList.add(this.tag);
         let arrowSpace = document.createElement("div");
+        arrowSpace.classList.add(this.tag);
         arrowSpace.classList.add("InventorySortControls__order___2snExpX");
         title.textContent = "COD";
         codeButton.appendChild(title);
         codeButton.appendChild(arrowSpace);
-        codeButton.addEventListener("click", () => {
-          this.sortByCode = true;
-        });
+        codeButton.addEventListener('click', sortByCodeOn );
 
         e.appendChild(codeButton);
-      }
-    });
-  }
+      
+        this.cleanups.push(() => codeButton.removeEventListener('click', sortByCodeOn))
+
+        sortingButtons.forEach(button => {
+          this.cleanups.push(() => button.removeEventListener('click', sortByCodeOff));
+      });
+    }
 }
+    )}}
