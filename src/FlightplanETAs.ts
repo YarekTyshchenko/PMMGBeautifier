385126ff05eab8cd29350591dd2b86201fcbeac4
hollo6 <dark@freemail.hu>
2021-05-06 17:47:32 +0200

    fixed flight plan ETA

--- src/FlightplanETAs.ts
+++ src/FlightplanETAs.ts
@@ -1,16 +1,16 @@
 import {convertDurationToETA, createTextSpan, genericCleanup} from "./util";
 
 export class FlightplanETAs {
-  private tag = "pb-flightplan-eta";
-  cleanup() {
-    genericCleanup(this.tag);
-  }
-  run() {
-    const elements = Array.from(document.querySelectorAll("table[class~='_2VAlxocH7EtoTdOjBzxulS']"));
-    elements.forEach(tbody => {
-      const targetRow = tbody.children[0].children[3];
-      const eta = convertDurationToETA(targetRow.children[0].textContent);
-      targetRow.appendChild(createTextSpan(` (${eta})`, this.tag))
-    })
-  }
+    private tag = "pb-flightplan-eta";
+    cleanup() {
+        genericCleanup(this.tag);
+    }
+    run() {
+        const elements = Array.from(document.querySelectorAll("table[class='_1g8dfBRzDAqRXFqRvSaL75 _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ'] > tbody > tr"));
+        elements.forEach(destinationRow => {
+            const targetRow = destinationRow.children[3];
+            const eta = convertDurationToETA(targetRow.children[0].textContent);
+            targetRow.appendChild(createTextSpan(` (${eta})`, this.tag))
+        })
+    }
 }
