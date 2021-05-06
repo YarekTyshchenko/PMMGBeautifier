72428b132159d8ab6d2cb2c5d87a92f514a868e4
hollo6 <dark@freemail.hu>
2021-05-06 16:38:49 +0200

    moved selectors to module, changed summed ETA to factor in the number of buildings

--- src/OrderETAs.ts
+++ src/OrderETAs.ts
@@ -15,21 +15,25 @@ export class OrderETAs {
     private beautifyAggregateProductionQueue() {
         const prodLines = Array.from(document.querySelectorAll(Selector.ProdLine));
         prodLines.forEach(line => {
-            const prodItems = Array.from(line.querySelectorAll(Selector.ProdLineItem));
-            let sumTime = 0;
+            const prodItems = Array.from(line.querySelectorAll("div[class='_1a75pC9Q0YF44bObHykWIA']"));
+            let sumTimes = Array();
             for (let i = 0; i < prodItems.length; i++) {
-                const itemETA = (prodItems[i].querySelector(Selector.ProdLineItemData + " > span"));
+                const itemETA = (prodItems[i].querySelector("div[class='_2wCEB4yaom4TdA4cxLZhbr'] div[class='_1j-lU9fMFzEgedyKKsPDtL _3dW9W1Qi1zDylwVf7nNSih'] > span"));
                 if (itemETA) {
                     const progress = (prodItems[i].querySelector("span[class='E1aHYdg2zdgvZCsPl3p9y _3RsFeLwUgZ4bFiiA1fteEe']"));
-                    sumTime = sumTime + parseDuration(itemETA.textContent);
-                    const eta = convertParsedDurationToETA(sumTime);
-                    const etaTag = createTextSpan(` (${eta})`, this.tag);
-                    if (progress) {
-                        etaTag.style.color = "#b0b0b0";
-                        progress.appendChild(etaTag);   
+                    const etaValue = parseDuration(itemETA.textContent);
+                    if (progress) { // this item is already being produced, need to use the current value
+                        const eta = convertParsedDurationToETA(etaValue);
+                        const etaTag = createTextSpan(` (${eta})`, this.tag);
+                        progress.parentElement!.appendChild(etaTag);
+                        sumTimes.push(etaValue);
                     }
-                    else {
-                        prodItems[i].appendChild(etaTag);
+                    else { // item is in the queue, need to find the earliest slot it can start and add it there
+                        const lowestEta = Math.min(...sumTimes);
+                        const summedEta = lowestEta + etaValue;
+                        sumTimes[sumTimes.indexOf(lowestEta)] = summedEta;
+                        const eta = convertParsedDurationToETA(summedEta);
+                        prodItems[i].appendChild(createTextSpan(` (${eta})`, this.tag));
                     }
                 }
             }
