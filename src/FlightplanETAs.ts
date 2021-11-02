fcbd6a56d05cfc4b38319f0c4c3bcd0695aa4022
hollo6 <dark@freemail.hu>
2021-11-02 11:58:34 +0100

    fix for new version CXOS + FLT ETA

--- src/FlightplanETAs.ts
+++ src/FlightplanETAs.ts
@@ -6,9 +6,9 @@ export class FlightplanETAs {
         genericCleanup(this.tag);
     }
     run() {
-        const elements = Array.from(document.querySelectorAll("table[class='_1g8dfBRzDAqRXFqRvSaL75 _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ'] > tbody > tr"));
+        const elements = Array.from(document.querySelectorAll("div[class='_1T3GrusQ2ydTsNKeMaEfPl'] div > div > div > div > div > div > table > tbody > tr"));
         elements.forEach(destinationRow => {
-            const targetRow = destinationRow.children[3];
+            const targetRow = destinationRow.children[7];
             if (targetRow.children[0] && targetRow.children[0].textContent) {
                 const eta = convertDurationToETA(targetRow.children[0].textContent);
                 targetRow.appendChild(createTextSpan(` (${eta})`, this.tag))
--- src/Selector.ts
+++ src/Selector.ts
@@ -1,10 +1,10 @@
 export const Selector = {
     LMCommodityAdText: "div[class='_14L--Z4VrwQHE-Dayta1db']",
     LMCommodityAdInnerText: "div[class='_1owHJs3IjU2hxdT0zQ1ytB']",
-    CXOBTable: "table[class='_3hdJGl6KSop3ReUzeVtjD2 _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ']",
+    CXOBTable: "div[class='sTkR6wF4_U-Kmwu7C2eu1'] > table",
     FXOBTable: "table[class='e9tTFVletB4P36C7c3_3t _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ']",
     CXOrdersExchangeName: "span[class='_3ifriA33o8WAhdFSaIgqWi']",
-    CXOrdersTable: "table[class='_2HAbDk9YexRt4S5-IhEbvT _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ']",
+    CXOrdersTable: "div[class='Yi2NJnhSe78fsycfnagth _1v_haUkG0iwIeZkpE2Mkrh'] > div > div > div > div > div > div > div > table",
     ProdLineHeader: "div[class='_11WSNCrU67r9BAS8vuYINW _2NKqmMbW69tQxqvJOvKvLL']",
     ProdLineItem: "div[class~='z8O6A0dWYid_6Vb1y75qz']",
 }
