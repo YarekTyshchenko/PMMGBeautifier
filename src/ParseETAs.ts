7a62f050ef9c60e08adce2d913718b488009ecf3
hollo6 <dark@freemail.hu>
2021-11-03 12:10:37 +0100

    patch fix 3

--- src/ParseETAs.ts
+++ src/ParseETAs.ts
@@ -4,20 +4,26 @@ import {convertDurationToETA, createTextSpan, genericCleanup} from "./util";
  * Parse all ETA times and add the actual date-time of arrival
  */
 export class ParseETAs {
-  private tag = "pb-other-etas";
-  cleanup() {
-    genericCleanup(this.tag);
-  }
-  run() {
-      const ETARows = Array.from(document.querySelectorAll("div[class='_1T3GrusQ2ydTsNKeMaEfPl'] > div > div > div[class='_1JqhiJ8_SwKH8PRALcO9Hc _33A_5lETf4HBqwJi_q-jhZ _1vWRpdI8cKNMPyOPnzlXgX'] > div[class='shbi17MHX7PpkUzoWVbWm'] > div > div > div > table > tbody > tr"));
-      ETARows.forEach(row => {
-        // 7th cell contains flight time
-        const etaCell = row.querySelectorAll("td").item(7);
-        if (etaCell.textContent) {
-          const textContent = etaCell.textContent.split('(')[0];
-          const eta = convertDurationToETA(textContent);
-          etaCell.appendChild(createTextSpan(` (${eta})`, this.tag));
+		private tag = "pb-other-etas";
+		cleanup() {
+				genericCleanup(this.tag);
+		}
+		run() {
+				var xpath = "//div[@class='_3ax11KjMIqVfK5lZnj8GLw _32tEvYagqJBRvv-BrnI4J5'][text()='Fleet']";
+				var matchingBuffers = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+				for (var i = 0; i < matchingBuffers.snapshotLength; i++) {
+						var buffer = matchingBuffers.snapshotItem(i);
+						if (buffer && buffer.parentElement) {
+								var fleetBuffer = buffer.parentElement.parentElement!;
+								const etaCells = Array.from(fleetBuffer.querySelectorAll("div[class='_1JqhiJ8_SwKH8PRALcO9Hc _33A_5lETf4HBqwJi_q-jhZ _1vWRpdI8cKNMPyOPnzlXgX'] > div > div > div > div > table > tbody > tr > td:nth-of-type(8)"));
+								etaCells.forEach(etaCell => {
+										if (etaCell.textContent) {
+												const textContent = etaCell.textContent.split('(')[0];
+												const eta = convertDurationToETA(textContent);
+												etaCell.appendChild(createTextSpan(` (${eta})`, this.tag));
+										}
+								});
+            }
         }
-      });
-  }
+	}
 }
