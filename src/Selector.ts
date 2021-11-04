6b5e3bce853a9ce53138091e4583203bca4c983e
hollo6 <dark@freemail.hu>
2021-11-04 11:23:35 +0100

    fix CXOS weirdness with changed/unchanged class names

--- src/Selector.ts
+++ src/Selector.ts
@@ -4,7 +4,7 @@ export const Selector = {
     CXOBTable: "div[class='sTkR6wF4_U-Kmwu7C2eu1'] > table",
     FXOBTable: "table[class='e9tTFVletB4P36C7c3_3t _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ']",
     CXOrdersExchangeName: "span[class='_3ifriA33o8WAhdFSaIgqWi']",
-    CXOrdersTable: "div[class='Yi2NJnhSe78fsycfnagth _1v_haUkG0iwIeZkpE2Mkrh'] > div > div > div > div > div > div > div > table",
+    CXOrdersTable: "div[class~='_1v_haUkG0iwIeZkpE2Mkrh'] > div > div > div > div > div > div > div > table",
     ProdLineHeader: "div[class='_11WSNCrU67r9BAS8vuYINW _2NKqmMbW69tQxqvJOvKvLL']",
     ProdLineItem: "div[class~='z8O6A0dWYid_6Vb1y75qz']",
 }
