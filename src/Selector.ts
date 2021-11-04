7b8b3da82c03d47deca33f61e1f94fae4f5100ad
hollo6 <dark@freemail.hu>
2021-11-04 11:54:02 +0100

    fix CXOS vs. FLT clash

--- src/Selector.ts
+++ src/Selector.ts
@@ -4,7 +4,7 @@ export const Selector = {
     CXOBTable: "div[class='sTkR6wF4_U-Kmwu7C2eu1'] > table",
     FXOBTable: "table[class='e9tTFVletB4P36C7c3_3t _2Fog1ad46aZ4q-RoEgK3R6 _1vWRpdI8cKNMPyOPnzlXgX _33A_5lETf4HBqwJi_q-jhZ']",
     CXOrdersExchangeName: "span[class='_3ifriA33o8WAhdFSaIgqWi']",
-    CXOrdersTable: "div[class~='_1v_haUkG0iwIeZkpE2Mkrh'] > div > div > div > div > div > div > div > table",
+    CXOrdersTable: "div[class~='_1v_haUkG0iwIeZkpE2Mkrh'] > div[class='_2-M8WlI-JS7ws_xSL-0yYo'] > div[class='_1h7jHHAYnTmdWfZvSkS4bo'] > div:nth-of-type(3)[class='_1JqhiJ8_SwKH8PRALcO9Hc _33A_5lETf4HBqwJi_q-jhZ _1vWRpdI8cKNMPyOPnzlXgX'] > div[class='shbi17MHX7PpkUzoWVbWm'] > div > div[class='sTkR6wF4_U-Kmwu7C2eu1'] > div > table",
     ProdLineHeader: "div[class='_11WSNCrU67r9BAS8vuYINW _2NKqmMbW69tQxqvJOvKvLL']",
     ProdLineItem: "div[class~='z8O6A0dWYid_6Vb1y75qz']",
 }
