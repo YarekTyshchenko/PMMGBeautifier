9164fcdefb3f7bb864861a2bc81515edca099e4c
Yarek Tyshchenko <yarekt@gmail.com>
2022-02-12 12:16:09 +0000

    Fixup styles

--- src/Style.ts
+++ src/Style.ts
@@ -2,14 +2,14 @@
  * This is a list of CCS styles that can be applied to buttons and fonts
  */
 export const Style = {
-  Button: ["_1VPBeuov5AYlOu4s7pKIlY"],
-  ButtonPrimary: ["_1Y9l3J20Xn-CyxMZIcH06i"],
-  ButtonSuccess: ["_3yZx55zAhax66rAfv6d6Z1"],
-  ButtonDanger: ["_31dQZugJBAqjKvME7bRBlA"],
-  SidebarSectionHead: ["CCGkvrSnzJdaNZHYPPEHp", "_33A_5lETf4HBqwJi_q-jhZ"],
-  SidebarSectionContent: ["LmT6E6SUymEKlbZEY3tQ8", "_33A_5lETf4HBqwJi_q-jhZ"],
-  SidebarLine: ["_258LbKlZRnQLY888Zg1cXb", "_24sz11_G6VXEYloo9FtRtZ"],
-  FontsRegular: ["_1EHFMt11olvELFgH4xaNE8"],
+  Button: ["fMW62cERnlzxZPFhnlPOeQ=="],
+  ButtonPrimary: ["kgGsDNvDoWj61w4I7VAlfA=="],
+  ButtonSuccess: ["QW80xveQm2GESkSORRH24g=="],
+  ButtonDanger: ["ZFXWy4HCnztpZNlCXk83wQ=="],
+  SidebarSectionHead: ["_2YrOM7-2sdK042VvH6WaJg==", "fTT52i+1oFauxHOjVfGTww=="],
+  SidebarSectionContent: ["CN0NPNovlYtaIm4bqHFbLw==", "fTT52i+1oFauxHOjVfGTww=="],
+  SidebarLine: ["y84EUI8gCP-SV91X7vIihQ==", "fVd3aYJhFY-uuaH+QTByhA=="],
+  FontsRegular: ["CBorIbFC6Yt+FRYEHZyuaA=="],
 }
 
 export const WithStyles = (...style: string[][]): string[] => {
